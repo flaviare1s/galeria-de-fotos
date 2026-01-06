e83a2f1a829e23b444e9f08b7174212af6b41997
flaviare1s <flaviarster@gmail.com>
2026-01-06 11:09:08 -0300

    Ajusta cores do header em modo claro

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import ThemeToggle from './ThemeToggle';
 
 function Header() {
   return (
-    <header className="bg-linear-to-r from-purple-200 to-purple-300 dark:from-gray-800 dark:to-gray-900 text-white shadow-lg">
+    <header className="bg-linear-to-r from-purple-300 to-purple-600 dark:from-gray-800 dark:to-gray-900 text-white shadow-lg">
       <div className="max-w-7xl mx-auto px-4 py-8 text-center relative flex items-center justify-between">
         <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold drop-shadow-lg">
           Galeria de Fotos
